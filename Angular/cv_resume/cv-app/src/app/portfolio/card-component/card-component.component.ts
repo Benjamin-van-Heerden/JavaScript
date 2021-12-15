82a0784c70e4c2c9b1580b6e4f894e634852ef14
Benjamin-van-Heerden <benjaminvh1997@gmail.com>
2021-12-15 14:09:46 +0200

    Housekeeping commit of untracked files

--- Angular/cv_resume/cv-app/src/app/portfolio/card-component/card-component.component.ts
+++ Angular/cv_resume/cv-app/src/app/portfolio/card-component/card-component.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card-component',
   templateUrl: './card-component.component.html',
-  styleUrls: ['./card-component.component.css']
+  styleUrls: ['./card-component.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CardComponentComponent implements OnInit {
 
